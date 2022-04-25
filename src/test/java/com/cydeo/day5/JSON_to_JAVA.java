ffdfe1d3accffda0923b0f8a0b49a76e462f4e50
cross <crosstown11223@yahoo.com>
2022-04-25 14:55:59 -0400

    HrTestWithParameters completed

--- src/test/java/com/cydeo/day5/JSON_to_JAVA.java
+++ src/test/java/com/cydeo/day5/JSON_to_JAVA.java
@@ -12,6 +12,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -27,33 +28,45 @@ public class JSON_to_JAVA  extends SpartansTestBase {
      *
      */
 
-
-
-
     @DisplayName("GET one Spartan and deserialize to MAP")
     @Test
     public void test1(){
 
-    Response response = given()
-            .accept(ContentType.JSON)
-            .pathParam("id",15)
-            .when()
-            .get("/api/spartans/{id}")
-            .then()
-            .statusCode(200)
-            .extract().response();
+        Response response = given()
+                .accept(ContentType.JSON)
+                .pathParam("id",15)
+                .when()
+                .get("/api/spartans/{id}")
+                .then()
+                .statusCode(200)
+                .extract().response();
 
-    //get the json body and convert it to java map
+        //get the json body and convert it to java map
 
-    Map<String,Object> jsonMap = response.as(Map.class);
+        Map<String,Object> jsonMap = response.as(Map.class);
 
         System.out.println(jsonMap);
 
-    String name = (String) jsonMap.get("name");
-    //verify name is Meta
+        String name = (String) jsonMap.get("name");
+        //verify name is Meta
 
-    assertThat(name,is("Meta"));
-}
+        assertThat(name,is("Meta"));
+    }
+
+    @DisplayName("GET all spartans to JAVA data structure")
+    @Test
+    public void getAllSpartan(){
+
+        Response response = get("/api/spartans").then().statusCode(200).extract().response();
+
+        //we need to convert json to java which is deserialize
+
+        List<Map<String,Object>> jsonList = response.as(List.class);
+
+        System.out.println(jsonList.get(15).get("gender"));
+
+    }
+
 
 
 
