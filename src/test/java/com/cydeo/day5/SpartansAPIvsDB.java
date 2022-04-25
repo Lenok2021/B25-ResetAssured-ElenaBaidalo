4c508d22ef21f0b73683be8aef86d234c27916c8
cross <crosstown11223@yahoo.com>
2022-04-25 15:52:35 -0400

    HrTestWithParameters completed

--- src/test/java/com/cydeo/day5/SpartansAPIvsDB.java
+++ src/test/java/com/cydeo/day5/SpartansAPIvsDB.java
@@ -1,10 +1,46 @@
 package com.cydeo.day5;
 
 import com.cydeo.utilities.SpartansTestBase;
+import com.cydeo.utilitiesDB.DB_Util;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
 
 public class SpartansAPIvsDB extends SpartansTestBase {
 
+    @DisplayName("Compare one spartan info api vs db")
+    @Test
+    public void test1() {
 
+        Response response =
+                given()
+                        .accept(ContentType.JSON)
+                        .pathParam("id", 15)
+                        .when()
+                        .get("/api/spartans/{id}")
+                        .then()
+                        .extract().response();
+        // convert JSON response to MAP
+
+        Map<String, Object> apiMAP = response.as(Map.class);
+        System.out.println("apiMAP = " + apiMAP);
+
+        // we need to get information from DB
+        // ADD  dependency  from  Oracle
+        // ADD  DB_utilities
+
+
+
+
+
+    }
 
 
 }
--- src/test/java/com/cydeo/utilities/SpartansTestBase.java
+++ src/test/java/com/cydeo/utilities/SpartansTestBase.java
@@ -1,7 +1,10 @@
 package com.cydeo.utilities;
 
+import com.cydeo.utilitiesDB.DB_Util;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 public class SpartansTestBase {
 
@@ -10,6 +13,21 @@ public class SpartansTestBase {
     @BeforeAll
     public static void init(){
         RestAssured.baseURI ="http://54.234.53.165:8000";
+
+        String urlSP = "jdbc:oracle:thin:@54.234.53.165:1521:XE";
+        String usernameSP = "SP";
+        String passwordSP = "SP";
+
+        DB_Util.createConnection(urlSP,usernameSP,passwordSP);
+
+
+
+
+    }
+
+    @AfterAll
+    public static void close(){
+        DB_Util.destroy();
     }
 
 
