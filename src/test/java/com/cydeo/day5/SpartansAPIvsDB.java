920b61c50c9798fbf5c9e08b698e49f76e7adf37
cross <crosstown11223@yahoo.com>
2022-04-25 18:56:24 -0400

    HrTestWithParameters completed

--- src/test/java/com/cydeo/day5/SpartansAPIvsDB.java
+++ src/test/java/com/cydeo/day5/SpartansAPIvsDB.java
@@ -31,35 +31,37 @@ public class SpartansAPIvsDB extends SpartansTestBase {
     @Test
     public void test1() {
 
-        Response response = given().accept(ContentType.JSON)
-                .pathParam("id", 15)
-                .when()
-                .get("/api/spartans/{id}")
-                .then()
-                .statusCode(200).extract().response();
+                  //we need to get api information
+            Response response = given().accept(ContentType.JSON)
+                    .pathParam("id", 15)
+                    .when()
+                    .get("/api/spartans/{id}")
+                    .then()
+                    .statusCode(200).extract().response();
 
-        //how to convert json response to map
-        Map<String,Object> apiMap = response.as(Map.class);
-        System.out.println(apiMap.toString());
+            //how to convert json response to map
+            Map<String,Object> apiMap = response.as(Map.class);
+            System.out.println(apiMap.toString());
 
-        String query = "SELECT SPARTAN_ID,NAME,GENDER,PHONE\n" +
-                "FROM SPARTANS\n" +
-                "WHERE SPARTAN_ID = 15";
+            //we need to get information from database
+            //which db we will connect ? oracle so we added depedency
+            //we need connection String of spartan db
+            String query = "SELECT SPARTAN_ID,NAME,GENDER,PHONE\n" +
+                    "FROM SPARTANS\n" +
+                    "WHERE SPARTAN_ID = 15";
 
-        Map<String, Object> dbMap = DBUtils.getRowMap(query);
+            Map<String, Object> dbMap = DBUtils.getRowMap(query);
 
-        System.out.println(dbMap);
+            System.out.println(dbMap);
+            //compare api vs db
+            assertThat(apiMap.get("id").toString(),is(dbMap.get("SPARTAN_ID").toString()));
+            assertThat(apiMap.get("name"),is(dbMap.get("NAME")));
+            assertThat(apiMap.get("gender"),is(dbMap.get("GENDER")));
+            assertThat(apiMap.get("phone").toString(),is(dbMap.get("PHONE").toString()));
 
-        // assertThat(apiMap.get("id").toString(), is(dbMap.get("Spartan_id").toString()));
-        assertThat(apiMap.get("id").toString(),is(dbMap.get("SPARTAN_ID").toString()));
-        assertThat(apiMap.get("name"),is(dbMap.get("NAME")));
-        assertThat(apiMap.get("gender"),is(dbMap.get("GENDER")));
-        assertThat(apiMap.get("phone").toString(),is(dbMap.get("PHONE").toString()));
-
-
-
+        }
 
     }
 
 
-}
+
