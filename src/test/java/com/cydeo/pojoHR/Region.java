df1a0282549aa0a220656974bf478a25d27c2212
cross <crosstown11223@yahoo.com>
2022-04-26 12:53:52 -0400

    Pojo

--- src/test/java/com/cydeo/pojoHR/Region.java
+++ src/test/java/com/cydeo/pojoHR/Region.java
@@ -1,6 +1,16 @@
 package com.cydeo.pojoHR;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.util.List;
+
+@Getter
+@Setter
+@ToString
+
+
 
 public class Region {
 
--- src/test/java/com/cydeo/pojoSpartans/SearchSpartans.java
+++ src/test/java/com/cydeo/pojoSpartans/SearchSpartans.java
@@ -1,6 +1,15 @@
 package com.cydeo.pojoSpartans;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.util.List;
+
+@Getter
+@Setter
+@ToString
+
 
 public class SearchSpartans {
 
@@ -11,23 +20,5 @@ public class SearchSpartans {
         return content;
     }
 
-    public void setContent(List<Spartan> content) {
-        this.content = content;
-    }
 
-    public int getTotalElement() {
-        return totalElement;
-    }
-
-    public void setTotalElement(int totalElement) {
-        this.totalElement = totalElement;
-    }
-
-    @Override
-    public String toString() {
-        return "Search{" +
-                "content=" + content +
-                ", totalElement=" + totalElement +
-                '}';
-    }
 }
